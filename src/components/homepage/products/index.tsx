04936bf9df69284b6cd6cfe0b416e14da4a68370
charlesalmeiida <charlesalmeida844@gmail.com>
2025-01-15 18:06:01 -0300

    feat: change the fetch products into a server component

--- src/components/homepage/products/index.tsx
+++ src/components/homepage/products/index.tsx
@@ -1,38 +1,10 @@
-"use client"
-
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
-import "swiper/css/pagination"
-import { CardItem } from "./cardItem"
-import { Pagination } from "swiper/modules"
-import { useEffect, useState } from "react"
 import { api } from "@/services/api"
+import { SlideProducts } from "./slideProducts"
 
-interface Product {
-  id: string
-  name: string
-  price: {
-    value: number
-    discount: number
-  }
-  soldout: boolean
-  image: string
-  description: string
-}
+export async function Products() {
+  const data = await api.get("shoes")
 
-export function Products() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    api
-      .get("/shoes")
-      .then((response) => {
-        setProducts(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
+  const products = await data.data
 
   return (
     <section className="py-28">
@@ -45,51 +17,7 @@ export function Products() {
             Conferir tudo
           </button>
         </div>
-        <div className="mt-14">
-          <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-              el: ".custom-pagination",
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
-            <ul>
-              {products.map((product) => (
-                <li key={product.id}>
-                  <SwiperSlide>
-                    <CardItem
-                      name={product.name}
-                      price={product.price.value}
-                      image={product.image}
-                    />
-                  </SwiperSlide>
-                </li>
-              ))}
-            </ul>
-            <div className="custom-pagination" />
-          </Swiper>
-        </div>
-        <div className="custom-pagination flex mt-11 justify-center"></div>
-        <style jsx global>{`
-          .custom-pagination .swiper-pagination-bullet {
-            width: 44px;
-            height: 5px;
-            background-color: #383838;
-            margin-right: 8px;
-            opacity: 0.3;
-            border-radius: 7px;
-            transition: all 0.3s ease;
-            cursor: pointer;
-          }
-          .custom-pagination .swiper-pagination-bullet-active {
-            background-color: #383838;
-            opacity: 1;
-            width: 48px;
-          }
-        `}</style>
+        <SlideProducts productData={products} />
       </div>
     </section>
   )
---
+++ src/components/homepage/products/slideProducts/index.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/pagination"
+import { Pagination } from "swiper/modules"
+import { CardItem } from "../cardItem"
+import { useState } from "react"
+
+interface Product {
+  id: string
+  name: string
+  price: {
+    value: number
+    discount: number
+  }
+  soldout: boolean
+  image: string
+  description: string
+}
+
+interface SlideProps {
+  productData: Product[]
+}
+
+export function SlideProducts({ productData }: SlideProps) {
+  const [products, setProducts] = useState<Product[]>([])
+
+  if (products.length === 0) {
+    setProducts(productData)
+  }
+
+  return (
+    <>
+      <div className="mt-14">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+            el: ".custom-pagination",
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <ul>
+            {products.slice(0, 8).map((product) => (
+              <li key={product.id}>
+                <SwiperSlide>
+                  <CardItem
+                    name={product.name}
+                    price={product.price.value}
+                    image={product.image}
+                  />
+                </SwiperSlide>
+              </li>
+            ))}
+          </ul>
+          <div className="custom-pagination" />
+        </Swiper>
+      </div>
+      <div className="custom-pagination flex mt-11 justify-center"></div>
+      <style jsx global>{`
+        .custom-pagination {
+          display: flex;
+          justify-content: center;
+          margin-top: 40px;
+        }
+        .custom-pagination .swiper-pagination-bullet {
+          width: 44px;
+          height: 5px;
+          background-color: #383838;
+          margin-right: 8px;
+          opacity: 0.3;
+          border-radius: 7px;
+          transition: all 0.3s ease;
+          cursor: pointer;
+        }
+        .custom-pagination .swiper-pagination-bullet-active {
+          background-color: #383838;
+          opacity: 1;
+          width: 48px;
+        }
+      `}</style>
+    </>
+  )
+}
