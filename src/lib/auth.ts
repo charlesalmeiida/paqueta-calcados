0cd195da472a2fa56a532770314c46766dbf5727
charlesalmeiida <charlesalmeida844@gmail.com>
2025-02-05 14:51:50 -0300

    refactor: mudando a estratégia de login para priorizar apenas o login com o google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import Apple from "next-auth/providers/apple"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Apple],
+  providers: [Google],
 })
 
 export async function signInWithGoogle() {
   return signIn("google")
-}
-
-export async function signInWithApple() {
-  return signIn("apple")
 }
